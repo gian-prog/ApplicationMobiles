537c1a95576a0f3985ff62fe6e01381085acd972
Giancarlo Benedetti <6262557@cegepmontpetit.ca>
2025-01-30 09:01:27 -0500

    FCT: FIN MULTI

---
+++ 1.2/Multilingue/app/src/main/java/com/example/multiLinguePROG3/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.multiLinguePROG3
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.multiLinguePROG3.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
